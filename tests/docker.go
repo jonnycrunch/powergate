91fab4b94eeff7f700df0f71c8e8cc522d0822a2
Ignacio Hagopian <jsign.uy@gmail.com>
2020-07-22 15:30:57 -0300

    use ipfs 0.6.0 in tests
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/docker.go
+++ tests/docker.go
@@ -14,7 +14,7 @@ func LaunchIPFSDocker(t *testing.T) (*dockertest.Resource, func()) {
 	pool, err := dockertest.NewPool("")
 	require.NoError(t, err)
 
-	ipfsDocker, err := pool.Run("ipfs/go-ipfs", "v0.5.1", []string{"IPFS_PROFILE=test"})
+	ipfsDocker, err := pool.Run("ipfs/go-ipfs", "v0.6.0", []string{"IPFS_PROFILE=test"})
 	require.NoError(t, err)
 
 	err = ipfsDocker.Expire(180)
